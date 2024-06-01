47f37389908ed56c5a5342c1fffeebb924b5463d
Realeed <73318913+Realeed@users.noreply.github.com>
2024-06-01 20:08:21 +0400

    Delete EFR32_xG22_btmesh_soc_multielement_server directory

--- EFR32_xG22_btmesh_soc_multielement_server/GNU ARM v12.2.1 - Default/gecko_sdk_4.4.1/platform/common/src/sl_slist.d
+++
@@ -1,8 +0,0 @@
-gecko_sdk_4.4.1/platform/common/src/sl_slist.o: \
- ../gecko_sdk_4.4.1/platform/common/src/sl_slist.c \
- C:\Users\hsarg\SimplicityStudio\BTMesh\EFR32_xG22_btmesh_soc_multielement_server\gecko_sdk_4.4.1\platform\common\toolchain\inc/sl_gcc_preinclude.h \
- C:\Users\hsarg\SimplicityStudio\BTMesh\EFR32_xG22_btmesh_soc_multielement_server\gecko_sdk_4.4.1\platform\common\inc/sl_assert.h \
- C:\Users\hsarg\SimplicityStudio\BTMesh\EFR32_xG22_btmesh_soc_multielement_server\gecko_sdk_4.4.1\platform\common\inc/sl_slist.h
-C:\Users\hsarg\SimplicityStudio\BTMesh\EFR32_xG22_btmesh_soc_multielement_server\gecko_sdk_4.4.1\platform\common\toolchain\inc/sl_gcc_preinclude.h:
-C:\Users\hsarg\SimplicityStudio\BTMesh\EFR32_xG22_btmesh_soc_multielement_server\gecko_sdk_4.4.1\platform\common\inc/sl_assert.h:
-C:\Users\hsarg\SimplicityStudio\BTMesh\EFR32_xG22_btmesh_soc_multielement_server\gecko_sdk_4.4.1\platform\common\inc/sl_slist.h:
--- EFR32_xG22_btmesh_soc_multielement_server/GNU ARM v12.2.1 - Default/gecko_sdk_4.4.1/platform/service/system/src/sl_system_init.d
+++
@@ -1,4 +0,0 @@
-gecko_sdk_4.4.1/platform/service/system/src/sl_system_init.o: \
- ../gecko_sdk_4.4.1/platform/service/system/src/sl_system_init.c \
- C:\Users\hsarg\SimplicityStudio\BTMesh\EFR32_xG22_btmesh_soc_multielement_server\gecko_sdk_4.4.1\platform\common\toolchain\inc/sl_gcc_preinclude.h
-C:\Users\hsarg\SimplicityStudio\BTMesh\EFR32_xG22_btmesh_soc_multielement_server\gecko_sdk_4.4.1\platform\common\toolchain\inc/sl_gcc_preinclude.h:
