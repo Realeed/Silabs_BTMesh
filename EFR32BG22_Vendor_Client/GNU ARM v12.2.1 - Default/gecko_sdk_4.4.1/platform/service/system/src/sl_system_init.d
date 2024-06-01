eef8e6cf04f8a422dbf7795332d9350ddf74f9be
Realeed <73318913+Realeed@users.noreply.github.com>
2024-06-01 20:08:00 +0400

    Delete EFR32BG22_Vendor_Client directory

--- EFR32BG22_Vendor_Client/GNU ARM v12.2.1 - Default/gecko_sdk_4.4.1/platform/service/system/src/sl_system_init.d
+++
@@ -1,4 +0,0 @@
-gecko_sdk_4.4.1/platform/service/system/src/sl_system_init.o: \
- ../gecko_sdk_4.4.1/platform/service/system/src/sl_system_init.c \
- C:\Users\hsarg\SimplicityStudio\BTMesh\EFR32BG22_Vendor_Client\gecko_sdk_4.4.1\platform\common\toolchain\inc/sl_gcc_preinclude.h
-C:\Users\hsarg\SimplicityStudio\BTMesh\EFR32BG22_Vendor_Client\gecko_sdk_4.4.1\platform\common\toolchain\inc/sl_gcc_preinclude.h:
